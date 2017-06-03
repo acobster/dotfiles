075c36600c03f05fe1b189218e7fbc19d8b7bcc0
The Ambassador of Funk <acobster@gmail.com>
2017-06-02 22:31:55 -0700

    all the rails

---
+++ rails/puma-rails5.rb
@@ -0,0 +1,60 @@
+require_relative 'util/app_template_helper.rb'
+
+
+replace_readme do
+  project_name = ask('hey what\'s this project actually called?')
+  project_blurb = ask('terse blurb:')
+
+  readme_markdown = <<-MARKDOWN
+  # #{project_name}
+
+  #{project_blurb}
+
+  ## Tests
+
+  TODO
+
+  ## Dev environment
+
+  TODO
+
+  MARKDOWN
+  readme_markdown
+end
+
+
+prompt_for_user_scaffold
+prompt_for_additional_scaffolds
+
+
+# Prompt to overwrite arbitrary files with those present in a skeleton directory
+unless (skeleton = ask('use skeleton files? (enter to skip)')).blank?
+  SKELETON_ROOT = File.expand_path(File.dirname(__FILE__))+'/app-skeletons'
+  overwrite_with_skeleton!(SKELETON_ROOT+'/'+skeleton)
+end
+
+
+replace_gemfile do
+  add_source 'https://rubygems.org'
+
+  gem 'rails', '~> 5.1'
+  gem 'mysql2', '>= 0.3'
+  gem 'puma', '~> 3.7'
+  gem 'bcrypt', '~> 3.1'
+  gem 'slim', '~> 3.0'
+  gem 'slim-rails', '~> 3.1'
+  gem 'dotenv', '~> 2.2'
+  gem 'rails_admin', '~> 1.2'
+  gem 'listen', '~> 3.1'
+
+  gem_group :development, :test do
+    gem 'byebug', '~> 9.0'
+    gem 'rspec-rails', '~> 3.6'
+    gem 'webmock', '~> 3.0'
+    gem 'vcr', '~> 3.0'
+    gem 'capybara-webkit', '~> 1.14'
+  end
+end
+
+
+# TODO prompt for initializers?
---
+++ rails/util/app_template_helper.rb
@@ -0,0 +1,49 @@
+# Helper functions for app templates
+#
+def replace_readme(&block)
+  remove_file 'README.doc'
+  remove_file 'README.md'
+
+  create_file 'README.md'
+  append_file 'README.md', yield
+end
+
+
+def replace_gemfile(&block)
+  remove_file 'Gemfile'
+  create_file 'Gemfile'
+  yield
+
+  # obsess over whitespace
+  after_bundle do
+    gsub_file 'Gemfile', /^group\b/, "\ngroup"
+  end
+end
+
+
+def overwrite_with_skeleton!(root)
+  cwd = Dir.getwd
+  Dir.glob(root+'/**/*.*').each do |filepath|
+    puts project_filepath = filepath.sub(root, cwd)
+
+    # copy the skeleton file into the project tree
+    run "cp #{filepath} #{project_filepath}"
+  end
+end
+
+
+def prompt_for_user_scaffold
+  if yes?('generate user scaffold? (y/n)')
+    generate(:scaffold, 'user', 'first_name:string', 'last_name:string', 'email:string', 'username:string')
+  end
+end
+
+
+def prompt_for_additional_scaffolds
+  until (scaffold_name = ask('enter any additional scaffold name (enter to continue):')).blank?
+    columns = ask('enter space-separated columns (e.g. "foo:string bar:integer")')
+    send(:generate, :scaffold, scaffold_name, *columns.split(' '))
+  end
+end
+
+
