37c2c119a0d763dd978641c5f76e750036897faf
Coby Tamayo <ctamayo@sitecrafting.com>
2018-12-03 17:16:53 -0800

    snippets for directory trees

---
+++ snippets/wptests/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Configure our test environment
+ *
+ * @copyright 2018 SiteCrafting, Inc.
+ * @author    Coby Tamayo <ctamayo@sitecrafting.com>
+ */
+
+require_once __DIR__ . '/../vendor/autoload.php';
---
+++ snippets/wptests/cases/PostTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Test the CLASSNAME class
+ *
+ * @copyright 2018 SiteCrafting, Inc.
+ * @author    Coby Tamayo <ctamayo@sitecrafting.com>
+ */
+
+namespace NAMESPACE;
+
+class CLASSNAME extends Base {
+  public function test_create() {
+    $this->assertTrue(false); // FAIL
+  }
+}
