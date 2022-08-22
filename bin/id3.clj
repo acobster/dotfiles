eb15f35fe3f3467d4e15ac1c9335d05f4327bc61
Coby Tamayo <coby@tamayo.email>
2022-08-22 14:18:35 -0700

    fix id3 bugs, support --tracks-file

--- bin/id3.clj
+++ bin/id3.clj
@@ -7,20 +7,30 @@
     [clojure.string :as string]
     [clojure.tools.cli :as cli]))
 
-(defn- quoted [& ss]
-  (str "'" (apply str ss) "'"))
-
 (defn- zero-pad [n ct]
   (let [len (- (count (str ct)) (count (str n)))
         zeros (apply str (repeat len "0"))]
     (str zeros n)))
 
-(defn- output-name [in {:keys [ext track-count track-number number-tracks?]}]
-  (let [filename (string/join "." (butlast (string/split in #"\.")))
+(defn- regex [s]
+  (java.util.regex.Pattern/compile s))
+
+(defn- output-name [in {:keys [dir
+                               ext
+                               track-count
+                               track-number
+                               number-tracks?]}]
+  (let [dirpath (when (seq dir)
+                  (str dir java.io.File/separator))
         leftpad (when number-tracks?
-                  (str (zero-pad track-number track-count) "-"))]
+                  (str (zero-pad track-number track-count) "-"))
+        basename (last (string/split in (regex java.io.File/separator)))
+        filename (string/join "." (butlast (string/split basename #"\.")))
+        renamed (string/replace (string/lower-case (str filename "." ext))
+                                " " "-")]
     (str leftpad (string/replace (string/lower-case (str filename "." ext))
-                                 " " "-"))))
+                                 " " "-"))
+    (str dirpath leftpad renamed)))
 
 (defn- to-int [s]
   (try
@@ -39,36 +49,40 @@
 ;; ffmpeg -i Track\ 2.wav -map 0 -write_id3v2 1 -metadata 'artist=Mark Isham' -metadata 'album=Tibet' -metadata 'track=2/5' -metadata 'title=Part II' -y 02.flac
 (defn- convert [{:keys [in
                         metadata
+                        dir
                         output-format
                         number-tracks?
                         track-number
-                        track-count]}]
+                        track-count
+                        track-titles]}]
   (let [metadata-opts
         (reduce (fn [opts [k v]]
                   (if v
-                    (concat opts ["-metadata" (quoted (name k) "=" v)])
+                    (concat opts ["-metadata" [(name k) "=" v]])
                     opts))
                 [] metadata)
-        out (output-name in {:ext (name (or output-format "flac"))
+        out (output-name in {:dir dir
+                             :ext (name (or output-format "flac"))
                              :number-tracks? number-tracks?
                              :track-count track-count
                              :track-number track-number})
         command (concat
-                  ["ffmpeg" "-y" "-i" in "-map" "0" "-write_id3v2" "1"]
+                  ["ffmpeg" "-y" "-i" [in] "-map" "0" "-write_id3v2" "1"]
                   metadata-opts
                   [out])]
     command))
 
+(defn- quoted [ss]
+  (str "'" (apply str ss) "'"))
+
 (defn- run-commands! [run? cmds]
   (doseq [cmd cmds]
-    (println (string/join " " cmd))
-    (when run?
-      (apply sh cmd)
-      ;; TODO
-      #_
-      (let [{:keys [out err exit]} (apply sh cmd)]
-        (when err (.write *err* (str "ERROR: " err)))
-        (.write *out* out)))))
+    (let [human-readable (map #(if (vector? %) (quoted %) %) cmd)
+          cmd (map #(if (vector? %) (apply str %) %) cmd)]
+      (println (string/join " " human-readable))
+      (when run?
+        ;; TODO real error handling and such...
+        (apply sh cmd)))))
 
 (def cli-options
   [["-h" "--help" "Show usage"]
@@ -79,10 +93,12 @@
     :default false]
    [nil "--sort" "Sort by file name"]
    [nil "--dry-run" "Output commands to be run, but do not run them"]
-   ["-f" "--output-format FORMAT" "Destination format"
+   ["-f" "--output-format FORMAT" "destination format"
     :default :flac]
-   ["-t" "--track-file example.txt" "Description File"
-    :validate [(comp boolean io/resource) "File not found"]]])
+   ["-d" "--dir DIR" "destination directory"
+    :default "."]
+   ["-t" "--tracks-file example.txt" "Tracks file (track titles, in order)"
+    :validate [(comp boolean slurp) "File not found"]]])
 
 (defn main [args]
   (let [{:keys [options errors summary]} (cli/parse-opts args cli-options)]
@@ -93,19 +109,29 @@
       (println summary)
       :default
       (let [lines (string/split (slurp *in*) #"\n")
+            track-titles (when (:tracks-file options)
+                           (into {} (map-indexed
+                                      #(vector %1 %2)
+                                      (string/split
+                                        (slurp (:tracks-file options))
+                                        #"\n"))))
             track-count (or (:track-count options) (count lines))]
         (->> lines
              (map-indexed
                (fn [idx line]
-                 {:in line
-                  :output-format (:output-format options)
-                  :number-tracks? (:number-tracks options)
-                  :track-count track-count
-                  :track-number (or (extract-track-number line) (inc idx))
-                  :metadata
-                  {:artist (:artist options)
-                   :album (:album options)
-                   :track (str (inc idx) "/" track-count)}}))
+                 (let [track-number (or (extract-track-number line) (inc idx))]
+                   {:in line
+                    :dir (:dir options)
+                    :output-format (:output-format options)
+                    :number-tracks? (:number-tracks options)
+                    :track-count track-count
+                    :track-number track-number
+                    :metadata
+                    {:artist (:artist options)
+                     :album (:album options)
+                     :track (str (inc idx) "/" track-count)
+                     :title (when track-titles
+                              (get track-titles (dec track-number)))}})))
              (maybe-sort-by-track-number (:sort options))
              (map convert)
              (run-commands! (not (:dry-run options))))))))
