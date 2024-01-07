ebc7f15102a2e3c8860880cded7f76ea0bdf83ef
Coby Tamayo <coby@tamayo.email>
2024-01-07 00:39:22 -0800

    start configuring Git with home-manager

--- nixos/home-manager/tamayo.nix
+++ nixos/home-manager/tamayo.nix
@@ -13,8 +13,19 @@
     EDITOR = "nvim";
   };
 
-  # Let Home Manager install and manage itself.
-  programs.home-manager.enable = true;
-
   home.stateVersion = "22.11";
+
+  programs = {
+    # Let Home Manager install and manage itself.
+    home-manager.enable = true;
+
+    git = {
+      enable = true;
+      userName = "Coby Tamayo";
+      userEmail = "coby@tamayo.email";
+      aliases = {
+        st = "status";
+      };
+    };
+  };
 }
