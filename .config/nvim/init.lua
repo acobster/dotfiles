0963814f1df8614a7f2dabc24088c8e13af86f85
Coby Tamayo <coby@tamayo.email>
2024-01-07 01:40:56 -0800

    temporarily "fix" neovim config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,7 +12,7 @@ vim.cmd([[
   endfunction
 
   call SourceIfExists("~/dotfiles/vim/common.vim")
-  call SourceIfExists("~/dotfiles/vim/nvim-plugins.vim")
+  "call SourceIfExists("~/dotfiles/vim/nvim-plugins.vim")
   call SourceIfExists("~/dotfiles/vim/syntastic.vim")
   call SourceIfExists("~/dotfiles/vim/color.vim")
   call SourceIfExists("~/dotfiles/vim/racket.vim")
@@ -45,20 +45,20 @@ vim.g.vim_markdown_folding_disabled = 1
 --------------------
 
 -- Load custom tree-sitter grammar for org filetype
-require('orgmode').setup_ts_grammar()
+-- require('orgmode').setup_ts_grammar()
 
 -- Tree-sitter configuration
-require'nvim-treesitter.configs'.setup {
-  -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
-  highlight = {
-    enable = true,
-    disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
-    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
-  },
-  ensure_installed = {'org'}, -- Or run :TSUpdate org
-}
+--require'nvim-treesitter.configs'.setup {
+--  -- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
+--  highlight = {
+--    enable = true,
+--    disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
+--    additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
+--  },
+--  ensure_installed = {'org'}, -- Or run :TSUpdate org
+--}
 
-require('orgmode').setup({
-  org_agenda_files = {'~/sync/org/*'},
-  org_default_notes_file = '~/syng/org/notes.org',
-})
+--require('orgmode').setup({
+--  org_agenda_files = {'~/sync/org/*'},
+--  org_default_notes_file = '~/syng/org/notes.org',
+--})
