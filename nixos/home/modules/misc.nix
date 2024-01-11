2078ce6ddaac2ad565ff5904eb49a06aabc4b826
Coby Tamayo <coby@tamayo.email>
2024-01-10 16:43:57 -0800

    install misc packages

--- nixos/home/modules/misc.nix
+++ nixos/home/modules/misc.nix
@@ -3,6 +3,15 @@
 {
   home.packages = with pkgs; [
     cowsay
+    ffmpeg
+    gimp
+    ledger
+    ledger-web
     lolcat
+    rpi-imager
+    signal-desktop
+    syncthing
+    ungoogled-chromium
+    zulip
   ];
 }
