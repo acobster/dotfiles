2929a100c05cf42f1aa43333963de1f106ce70ac
Coby Tamayo <coby@tamayo.email>
2024-01-10 16:34:57 -0800

    quarantine unfree packages

--- nixos/home/modules/misc.nix
+++ nixos/home/modules/misc.nix
@@ -4,12 +4,5 @@
   home.packages = with pkgs; [
     cowsay
     lolcat
-    discord
-    zoom-us
-  ];
-
-  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
-    "discord"
-    "zoom"
   ];
 }
