964f677230a3da8812f22b559e87cbe4a471da19
Coby Tamayo <coby@tamayo.email>
2024-01-10 16:58:54 -0800

    move most packages to home config

--- nixos/home/modules/misc.nix
+++ nixos/home/modules/misc.nix
@@ -2,16 +2,24 @@
 
 {
   home.packages = with pkgs; [
+    babashka
+    brave
+    clojure
     cowsay
     ffmpeg
+    firefox
     gimp
+    gnome.gnome-terminal
     ledger
     ledger-web
     lolcat
+    ripgrep
     rpi-imager
     signal-desktop
     syncthing
+    transmission
     ungoogled-chromium
+    vlc
     zulip
   ];
 }
