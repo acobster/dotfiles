61aac1a7ea63f9afe91c2b5bea87cd6de8d87bff
Coby Tamayo <coby@tamayo.email>
2024-01-11 00:10:11 -0800

    more cleanup

--- snippets/wptests/bootstrap.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Configure our test environment
- *
- * @copyright 2019 SiteCrafting, Inc.
- * @author    Coby Tamayo <ctamayo@sitecrafting.com>
- */
-
-require_once __DIR__ . '/../vendor/autoload.php';
