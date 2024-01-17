30b51a19315769c5a9c5ca48fe3d177d807cf43f
Coby Tamayo <coby@tamayo.email>
2024-01-17 11:05:09 -0800

    streamline system vs. user packages

--- nixos/home/modules/misc.nix
+++ nixos/home/modules/misc.nix
@@ -8,28 +8,26 @@
     cowsay
     ffmpeg
     firefox
+    fzf
     gimp
     gnome.gnome-terminal
+    jq
     ledger
     ledger-web
     lolcat
+    lua
     ripgrep
     rpi-imager
     signal-desktop
+    silver-searcher
     syncthing
+    tmux
     transmission
+    tree
     ungoogled-chromium
     vlc
-    zulip
-
-    # TODO
-    fzf
-    jq
-    lua
-    silver-searcher
-    tmux
-    tree
     wget
     xclip
+    zulip
   ];
 }
