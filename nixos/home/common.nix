3c5b34c8c9ea892d252ddf300d55ac8d4ff367b4
Coby Tamayo <coby@tamayo.email>
2024-01-17 11:21:21 -0800

    split out home configs per machine

---
+++ nixos/home/common.nix
@@ -0,0 +1,21 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "tamayo";
+  home.homeDirectory = "/home/tamayo";
+
+  imports = [
+    ./modules/bash.nix
+    ./modules/direnv.nix
+    ./modules/misc.nix
+    ./modules/neovim.nix
+    ./modules/git.nix
+    ./modules/ulauncher.nix
+    ./modules/unfree.nix
+  ];
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.stateVersion = "22.11";
+}
