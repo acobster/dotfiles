a7e93c91475dea595aa7fb76876d8b0149d1cc2c
Coby Tamayo <coby@tamayo.email>
2024-01-21 21:33:40 -0800

    debug-comment some code

--- nixos/system/common.nix
+++ nixos/system/common.nix
@@ -7,7 +7,7 @@
   imports = [
     ./modules/boot.nix
     ./modules/display.nix
-    ./modules/network.nix
+    #./modules/network.nix
     ./modules/packages.nix
     ./modules/peripherals.nix
   ];
