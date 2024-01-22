33674e1dcebb8e3844b8804f3c030058eb4169e8
Coby Tamayo <coby@tamayo.email>
2024-01-21 23:35:08 -0800

    explicitly disable networking.wireless

--- nixos/system/modules/network.nix
+++ nixos/system/modules/network.nix
@@ -8,9 +8,8 @@
   networking.useDHCP = lib.mkDefault true;
   # networking.interfaces.enp0s3.useDHCP = lib.mkDefault true;
 
-  # Pick only one of the below networking options.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-  #networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
+  # Need to disable this for xserver.desktopManager.gnome to work.
+  networking.wireless.enable = false;
 
   # Set your time zone.
   time.timeZone = "America/Los_Angeles";
