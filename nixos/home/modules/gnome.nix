205426733a244963e6bb27ea2e4edef8710968b7
Coby Tamayo <coby@tamayo.email>
2024-01-27 03:35:43 -0800

    install gtk themes

---
+++ nixos/home/modules/gnome.nix
@@ -0,0 +1,37 @@
+{ config, pkgs, ... }:
+
+{
+  # https://determinate.systems/posts/declarative-gnome-configuration-with-nixos
+  gtk = {
+    enable = true;
+
+    theme = {
+      name = "Graphite";
+      package = pkgs.graphite-gtk-theme;
+    };
+
+    iconTheme = {
+      name = "Tela Circle";
+      package = pkgs.tela-circle-icon-theme;
+    };
+
+    cursorTheme = {
+      name = "Graphite cursors";
+      package = pkgs.graphite-cursors;
+    };
+
+    gtk3.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+
+    gtk4.extraConfig = {
+      Settings = ''
+        gtk-application-prefer-dark-theme=1
+      '';
+    };
+  };
+
+  home.sessionVariables.GTK_THEME = "Graphite";
+}
