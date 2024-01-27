0dba932b178578bdb46284b0453120ee95bdcf00
Coby Tamayo <coby@tamayo.email>
2024-01-27 10:05:41 -0800

    fix steam

--- flake.nix
+++ flake.nix
@@ -14,8 +14,15 @@
   outputs = { nixpkgs, home-manager, ... }:
   let
     system = "x86_64-linux";
-    pkgs = nixpkgs.legacyPackages.${system};
     lib = nixpkgs.lib;
+    pkgs = import nixpkgs {
+      inherit system;
+      config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "steam"
+        "steam-original"
+        "steam-run"
+      ];
+    };
     util = import ./nixos/util.nix {
       inherit system pkgs nixpkgs home-manager;
     };
