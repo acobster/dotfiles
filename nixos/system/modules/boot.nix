4ce683708e0e0fef025570986627088316e265c5
Coby Tamayo <coby@tamayo.email>
2024-03-14 19:23:27 -0700

    boot updates

--- nixos/system/modules/boot.nix
+++ nixos/system/modules/boot.nix
@@ -1,10 +1,22 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    efi.efiSysMountPoint = "/boot/efi";
+
+    grub = {
+      theme = pkgs.nixos-grub2-theme;
+      #backgroundColor = "#FFCC00";
+      backgroundColor = "#F0E3FF";
+      splashImage = ../../img/grub.png;
+    };
+  };
+
+  isoImage.splashImage = ../../img/live.png;
+  isoImage.efiSplashImage = ../../img/live.png;
 
   boot.initrd.availableKernelModules = [ "ata_piix" "ohci_pci" "ahci" "sd_mod" "sr_mod" ];
   boot.initrd.kernelModules = [ ];
