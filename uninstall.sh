fbab2365a98a59c94bab6e64f29c949b39a3aaa6
Coby Tamayo <coby@amperon.co>
2024-08-28 00:40:34 -0700

    more uninstall improvements

--- uninstall.sh
+++ uninstall.sh
@@ -5,15 +5,23 @@ if [[ "$confirm" != y ]] ; then
   exit 1
 fi
 
-sudo systemctl stop nix-daemon.service
-sudo systemctl disable nix-daemon.socket nix-daemon.service
-sudo systemctl daemon-reload
-sudo rm -rf /etc/nix /etc/profile.d/nix.sh /etc/tmpfiles.d/nix-daemon.conf /nix ~root/.nix-channels ~root/.nix-defexpr ~root/.nix-profile
+multi_user=$(nix-info | grep 'multi-user?: yes')
 
-for i in $(seq 1 32); do
-  sudo userdel nixbld$i
-done
-sudo groupdel nixbld
+if [[ "$multi_user" ]] ; then
+  echo 'tearing down multi-user installation...'
+  sudo systemctl stop nix-daemon.service
+  sudo systemctl disable nix-daemon.socket nix-daemon.service
+  sudo systemctl daemon-reload
+  sudo rm -rf /etc/nix /etc/profile.d/nix.sh /etc/tmpfiles.d/nix-daemon.conf /nix ~root/.nix-channels ~root/.nix-defexpr ~root/.nix-profile
+
+  for i in $(seq 1 32); do
+    sudo userdel nixbld$i
+  done
+  sudo groupdel nixbld
+else
+  echo 'tearing down single-user installation...'
+  sudo rm -rf /nix
+fi
 
 rm -rf ~/.bash_profile \
   ~/.bashrc \
