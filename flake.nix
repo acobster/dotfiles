eb5b5963e67b4fe9f75e93bc3153e301a9a23276
Coby Tamayo <coby@tamayo.email>
2024-12-26 20:34:49 -0800

    upgrade system channel to nixpkgs 24.11

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   description = ".";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-23.11";
