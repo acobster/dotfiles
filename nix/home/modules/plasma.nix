c7482a96a6d5e8250c1658c40ba0c3e19cdbb6d9
Coby Tamayo <coby@tamayo.email>
2024-12-27 23:11:13 -0800

    configure opacity

--- nix/home/modules/plasma.nix
+++ nix/home/modules/plasma.nix
@@ -27,8 +27,6 @@
     shortcuts = {
       "ksmserver"."Lock Session" = "Meta+L";
       "ksmserver"."Log Out" = "Ctrl+Alt+Del";
-      "kwin"."Increase Opacity" = "Shift+Alt+Up,Increase Opacity of Active Window by 5%";
-      "kwin"."Decrease Opacity" = "Shift+Alt+Down,Decrease Opacity of Active Window by 5%";
       "kwin"."Edit Tiles" = "Meta+T";
       "kwin"."Expose" = "Ctrl+F9,Meta+Tab";
       "kwin"."Grid View" = "Meta+G";
@@ -36,6 +34,10 @@
       "kwin"."Window Minimize" = "Meta+H";
       "kwin"."Window Quick Tile Bottom" = ["Meta+Shift+Down" "Meta+PgDown"];
       "kwin"."Window Quick Tile Top" = ["Meta+PgUp" "Meta+Shift+Up"];
+    };
+
+    configFile = {
+      "kwinrc"."MouseBindings"."CommandAllWheel" = "Change Opacity";
     };
   };
 }
