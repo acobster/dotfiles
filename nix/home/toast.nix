b94262e01e61fc1a7fefbbae974c153f16bfbdc3
Coby Tamayo <coby@tamayo.email>
2025-08-29 06:38:50 -0700

    clear up some space on toast

--- nix/home/toast.nix
+++ nix/home/toast.nix
@@ -8,18 +8,14 @@
   programs.home-manager.enable = true;
 
   home.packages = with pkgs; [
-    discord
     fzf
     curl
     jq
-    lua
-    ripgrep
     silver-searcher
     tree
     vlc
     wget
     xclip
-    zulip
   ];
 
   imports = [
@@ -33,7 +29,6 @@
     ./modules/gnome.nix
     ./modules/tmux.nix
     ./modules/ubuntu.nix
-    ./modules/ulauncher.nix
   ];
 
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (pkgs.lib.getName pkg) [
