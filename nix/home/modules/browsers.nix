8a464bd3db37bf03cc705ef14eea67dd3b0024aa
Coby Tamayo <coby@tamayo.email>
2025-09-11 18:00:27 -0700

    default to DuckDuckGo in ff

--- nix/home/modules/browsers.nix
+++ nix/home/modules/browsers.nix
@@ -28,18 +28,23 @@
         "extensions.activeThemeID" = "firefox-compact-dark@mozilla.org";
       };
 
-      search.engines = {
-        "Nix Packages" = {
-          urls = [{
-            template = "https://search.nixos.org/packages";
-            params = [
-              { name = "type"; value = "packages"; }
-              { name = "query"; value = "{searchTerms}"; }
-            ];
-          }];
+      search = {
+        default = "DuckDuckGo";
+        privateDefault = "DuckDuckGo";
 
-          icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
-          definedAliases = [ "@nix" ];
+        engines = {
+          "Nix Packages" = {
+            urls = [{
+              template = "https://search.nixos.org/packages";
+              params = [
+                { name = "type"; value = "packages"; }
+                { name = "query"; value = "{searchTerms}"; }
+              ];
+            }];
+
+            icon = "${pkgs.nixos-icons}/share/icons/hicolor/scalable/apps/nix-snowflake.svg";
+            definedAliases = [ "@nix" ];
+          };
         };
       };
 
