f69a7564e90be874169ee8d03b2b70ead7f33f35
Coby Tamayo <coby@tamayo.email>
2025-09-15 12:01:03 -0700

    more firefox customizations

--- nix/home/modules/browsers.nix
+++ nix/home/modules/browsers.nix
@@ -1,17 +1,24 @@
 { pkgs, inputs, ... }:
 
+let
+  lock-false = { Value = false; Status = "locked"; };
+  lock-true = { Value = true; Status = "locked"; };
+  lock-empty = { Value = ""; Status = "locked"; };
+in
 {
   home.packages = with pkgs; [
     brave
     ungoogled-chromium
   ];
 
+  # https://discourse.nixos.org/t/combining-best-of-system-firefox-and-home-manager-firefox-settings/37721
   programs.firefox = {
     enable = true;
 
     languagePacks = [ "en-US" "es" "fr" "ar" ];
 
     # See about:policies#documentation
+    # See also: https://mozilla.github.io/policy-templates/
     policies = {
       DisableTelemetry = true;
       DisablePocket = true;
@@ -20,6 +27,22 @@
         Locked = true;
         Cryptomining = true;
         Fingerprinting = true;
+      };
+
+      Preferences = {
+        "browser.newtabpage.pinned" = lock-empty;
+        "browser.newtabpage.activity-stream.discoverystream.spoc.impressions" = lock-empty;
+        "browser.newtabpage.activity-stream.feeds.section.topstories" = lock-false;
+        "browser.newtabpage.activity-stream.feeds.section.topsites" = lock-false;
+        "browser.newtabpage.activity-stream.impressionId" = lock-empty;
+        "browser.newtabpage.activity-stream.showSponsored" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsoredCheckboxes" = lock-false;
+        "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
+        "browser.newtabpage.activity-stream.showWeather" = lock-true;
+        "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
+        "browser.startup.homepage" = "https://isitbandcampfriday.com/";
+        "browser.topsites.contile.enabled" = lock-false;
+        "extensions.pocket.enabled" = lock-false;
       };
     };
 
