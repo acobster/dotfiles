9fd08dd2912e2ebb243951b717cc10a8c69c136b
Coby Tamayo <coby@tamayo.email>
2026-06-27 01:49:56 -0700

    distill, critical

--- bin/sense.clj
+++ bin/sense.clj
@@ -22,24 +22,28 @@
     :fan1-rpm [:framework_laptop-isa-000f :fan1 :fan1_input]
     :fan2-rpm [:framework_laptop-isa-000f :fan2 :fan2_input]}})
 
+(defn distill [profile data]
+  (into {} (map (juxt key #(get-in data (val %)))) profile))
+
 (comment
   (shell/sh "ls" "-l")
   (get-sensor-data)
+  (distill (get $profiles "clementine") (get-sensor-data))
   (-main)
   ,)
 
 (defn -main []
-  (let [sensor-data (get-sensor-data)
-        host (hostname)
-        profile (get $profiles host)
+  (let [hostname' (hostname)
+        profile (get $profiles hostname')
         _ (when-not profile (do
-                              (println "No profile found for" host)
+                              (println "No profile found for" hostname')
                               (System/exit 1)))
         {:keys [cpu-temp fan1-rpm fan2-rpm]}
-        (into {} (map (juxt key #(get-in sensor-data (val %))))
-              (get $profiles "clementine"))]
-    (println "CPU temp:" cpu-temp "°C")
-    (println "Fan 1:" fan1-rpm "RPM")
-    (println "Fan 2:" fan2-rpm "RPM")))
+        (distill profile (get-sensor-data))
+        critical? (>= cpu-temp 85)
+        temp-icon (if critical? "‼️" "✔️")]
+    (println temp-icon " CPU temp:" cpu-temp "°C")
+    (println "   Fan 1:" fan1-rpm "RPM")
+    (println "   Fan 2:" fan2-rpm "RPM")))
 
 (-main)
