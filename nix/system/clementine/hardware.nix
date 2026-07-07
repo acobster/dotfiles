a88a627ce1c0ffc79e6876c7145a1432c305caf0
Coby Tamayo <coby@tamayo.email>
2026-07-06 22:27:26 -0700

    upgrade systemd sleep settings

--- nix/system/clementine/hardware.nix
+++ nix/system/clementine/hardware.nix
@@ -74,10 +74,10 @@
   };
 
   # Disable suspension entirely:
-  systemd.sleep.extraConfig = ''
-    AllowSuspend=no
-    AllowHibernation=no
-    AllowHybridSleep=no
-    AllowSuspendThenHibernate=no
-  '';
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = "no";
+    AllowHibernation = "no";
+    AllowHybridSleep = "no";
+    AllowSuspendThenHibernate = "no";
+  };
 }
